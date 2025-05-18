02e5de2355c870984edb0938f7081cb70c3deba4
Lumetas <pokh9864@gmail.com>
2025-05-18 17:50:37 +0500

    config fix

--- config.php
+++ config.php
@@ -1,34 +1,27 @@
 <?php
 return [
-	'server' => [
-		'customServer' => true,
-		'host' => '0.0.0.0',
-		'port' => 8000,
-		'initFunction' => 'return function (] { echo "server started!\\n"}',
+	"server" => [
+		"customServer" => false,
+		"host" => "0.0.0.0",
+		"port" => 8000
 	],
-	'database' => [
-		'driver' => 'sqlite',
-		'sqlite_path' => 'db.sqlite',
+	"database" => [
+		"driver" => "sqlite",
+		"sqlite_path" => __DIR__ . "/db.sqlite"
 	],
-	'storage' => [
-		'driver' => null,
-		'time' => 600,
+	"storage" => [
+		"driver" => null,
+		"time" => 20
 	],
 	'auth' => [
-		'table' => 'meract_users',
-		'login_fields' => [
-			0 => 'email',
-			1 => 'password',
-		],
-		'registration_fields' => [
-			0 => 'hui',
-			1 => 'penis',
-		],
-		'jwt_secret' => '6e50077d690921b2daedc233327b20ff8d9ccc0df10d8c93ee8ca75d540aca2a',
-		'tokens_table' => 'meract_tokens',
-		'cookie_name' => 'ebat',
+		'table' => 'meract_users',               // Таблица пользователей
+		'login_fields' => ['email', 'password'], // Поля для входа
+		'registration_fields' => ['email', 'password'], // Поля для регистрации
+		'jwt_secret' => 'your-strong-secret',    // Секретный ключ для JWT
+		'tokens_table' => 'meract_tokens',       // Таблица недействительных токенов
+		'cookie_name' => "AUTHTOKEN"            // Название cookie
 	],
-	'morph' => [
-		'live' => 'e630814aedb50c95cc3538714ceafe034eacf582255e9f52168f24205a17fdd4',
-	],
+	"morph" => [
+		"live" => "super secret key"
+	]
 ];
